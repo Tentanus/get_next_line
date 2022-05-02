d14d1dca4247faf7bd8dbcb76dc25e2a6558b82f
Martijn Weverling <mweverli@student.codam.nl>
2022-05-02 22:52:14 +0200

    bump Make/main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mweverli <mweverli@codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/18 15:46:33 by mweverli      #+#    #+#                 */
-/*   Updated: 2022/04/30 19:09:32 by mweverli      ########   odam.nl         */
+/*   Updated: 2022/05/02 19:31:19 by mweverli      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int main(void)
 //	char	*fn4 = "/Users/mweverli/files/get_next_line/Text_files/top_chars_network.txt";
 //	char	*fn5 = "/Users/mweverli/files/get_next_line/Text_files/EpisodeIV_dialogues.txt";
 
-	int i = 3;
+	int i = 4;
 	int		fd3 = open(fn3, O_RDONLY);
 //	int		fd4 = open(fn4, O_RDONLY);
 //	int		fd5 = open(fn5, O_RDONLY);
